40b8d711c0c6e6f21b4e924fc8c29e525b8028f6
濱口和希 <>
2018-06-21 23:38:24 +0900

    [feature]sleepmode

--- automute.sh
+++ automute.sh
@@ -1,5 +1,11 @@
-echo "wait ${1} and mute..."
+echo "wait ${1} seconds and mute..."
+sleep_flag=-s
 
+if [ "${2}" = ${sleep_flag} ]; then
+  echo "after ${1} seconds, this pc sleepmode..."
+fi
 sleep $1
 osascript -e 'set volume with output muted'
-
+if [ "${2}" = ${sleep_flag} ]; then
+  osascript -e 'tell application "Finder" to sleep'
+fi
