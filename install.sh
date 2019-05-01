74b43dc0e6b7ff8d1ba4ada00845c126b830d0c4
hama-matcha <hamamatcha@gmail.com>
2019-05-01 20:13:35 +0900

    :+1: superuserでの実行許可を聞く文章を作成

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-echo Copy automute.sh to /usr/local/bin/ 
-cp $PWD/automute.sh /usr/local/bin/automute
+## Copy automute.sh to /usr/local/bin/ ##
+echo "Need your password because copy automute.sh to /usr/local/bin/"
+sudo cp $PWD/automute.sh /usr/local/bin/automute
